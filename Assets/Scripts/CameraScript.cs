5767210a8d2ec8c4c20c07ebe5270b70907059f3
Caleb Kauffman <Caleb Kauffman>
2016-05-17 11:24:01 -0700

    Puzzle 7

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class CameraScript : MonoBehaviour {
+
+    WebCamTexture webcamTexture;
+    public RawImage rawimage;
+    void Start()
+    {
+        webcamTexture = new WebCamTexture();
+        rawimage.texture = webcamTexture;
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+
+    }
+
+    public void TakePhoto()
+    {
+
+        //yield return new WaitForSeconds(1);
+
+        Texture2D photo = new Texture2D(webcamTexture.width, webcamTexture.height);
+        photo.SetPixels(webcamTexture.GetPixels());
+        photo.Apply();
+
+        //Encode to a PNG
+        byte[] bytes = photo.EncodeToPNG();
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        File.WriteAllBytes(@"C:\Users\Caleb\Documents\Puzzles\Pictures\" + "photo.png", bytes);
+
+        
+    }
+
+    public void OnDrawClick()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DrawButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawButton : MonoBehaviour {
+
+    public GameObject draw_script;
+    bool enabled;
+	// Use this for initialization
+	void Start () {
+        enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClick()
+    {
+        if (!enabled)
+        {
+            draw_script.GetComponent<Drawing>().enabled = true;
+            enabled = true;
+        }
+       //else
+        //{
+           // draw_script.GetComponent<Drawing>().enabled = false;
+            //enabled = false;
+        //}
+    }
+}
