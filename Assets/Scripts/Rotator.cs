2c90aa81f0694c4e00036f05627babdc6f3908ea
Caleb Kauffman <Caleb Kauffman>
2016-05-31 14:47:48 -0700

    Added puzzle 8

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿//ATTACH THIS SCRIPT TO THE CUBE PICKUP OBJECTS PREFAB
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    //so the rotation is applied to them
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+    //Update is called once per frame
+    //so all operations should be adjusted by delta time
+    //delta time is 1/fps
+}
